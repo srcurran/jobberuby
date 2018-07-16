e4e33267ab74ff2a7009d9c086232cb1178eb394
srcurran <srcurran@gmail.com>
2018-07-16 10:10:31 -0400

    removed users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  root 'welcome#index'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
