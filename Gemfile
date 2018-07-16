849e54e804a7696e47e70f12e4c160ca9fc8a682
srcurran <srcurran@gmail.com>
2018-07-16 17:37:15 -0400

    updated structure to include password & data requirements

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rails', '~> 5.2.0'
 
 #bootstrap
 gem 'bootstrap-sass', '3.3.7'
+#bcrypt
+gem 'bcrypt', '3.1.12'
 
 # Use sqlite3 as the database for Active Record & prostgeSQL for prod
 group :production do
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/home',    to: 'static_pages#home'
-  get  '/contact',  to: 'static_pages#contact'
+  get  '/contact', to: 'static_pages#contact'
+  get '/signup',   to: 'users#new'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
