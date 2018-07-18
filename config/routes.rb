66f882e305a0ff1f0c14fda3776efda872ccbd2d
srcurran <srcurran@gmail.com>
2018-07-18 15:40:51 -0400

    updates to db schema

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   resources :users
-  resources :todos, only: [:create, :destroy]
+  resources :todos
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
